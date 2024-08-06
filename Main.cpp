e0878275160def8b6c3b02cddefa46d8b4cb898b
FakeGath <givanalvaradoath@gmail.com>
2024-08-06 17:51:33 -0600

    Solution for Data input and Output

---
+++ Main.cpp
@@ -0,0 +1,18 @@
+// adds the library iostream to the program
+#include <iostream>
+
+int main(){
+
+    //Declaration of variables age int and name string.
+    int age {29};
+    std:: string name;
+
+    // prints message Hello World!
+    std ::cout <<"Hello World!" << std:: endl;
+
+    //Prints Age
+    std :: cout << "Age : " << age << std :: endl;
+
+
+    
+}
