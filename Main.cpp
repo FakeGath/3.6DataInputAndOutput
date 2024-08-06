9cec5637fa3c44a137506248d5ac7becd2e2b6fc
FakeGath <givanalvaradoath@gmail.com>
2024-08-06 17:55:44 -0600

    Adds Error and Log message to the solution

--- Main.cpp
+++ Main.cpp
@@ -13,6 +13,8 @@ int main(){
     //Prints Age
     std :: cout << "Age : " << age << std :: endl;
 
-
+    //Prints Errors and Logs
+    std :: cerr << "Error message : Something is wrong"<< std :: endl;
+    std :: clog << "Log message : Something happend " << std :: endl;
     
 }
